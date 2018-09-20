6c13c0f562c52b9a98ce3e174e1319975777b6dd
Arun Kalwan <aruns1@hotmail.co.uk>
2018-09-20 14:40:28 +0100

    WET code cleaned up in JS

--- js/app.js
+++ js/app.js
@@ -23,80 +23,66 @@ function genRandomCard() {
   switch (random) {
     case 1:
       card[0];
-      // document.getElementById("cardNumber").innerHTML = "Ace";
       console.log("You have got an ace");
       console.log(random);
       break;
     case 2:
       card[1];
-      // document.getElementById("cardNumber").innerHTML = "2";
       console.log("You have got 2");
       console.log(random);
       break;
     case 3:
       card[2];
-      // document.getElementById("cardNumber").innerHTML = "3";
       console.log("You have got 3");
       console.log(random);
       break;
     case 4:
       card[3];
-      // document.getElementById("cardNumber").innerHTML = "4";
       console.log("You have got 4");
       console.log(random);
       break;
     case 5:
       card[4];
-      // document.getElementById("cardNumber").innerHTML = "5";
       console.log("You have got 5");
       console.log(random);
       break;
     case 6:
       card[5];
-      // document.getElementById("cardNumber").innerHTML = "6";
       console.log("You have got 6");
       console.log(random);
       break;
     case 7:
       card[6];
-      // document.getElementById("cardNumber").innerHTML = "7";
       console.log("You have got 7");
       console.log(random);
       break;
     case 8:
       card[7];
-      // document.getElementById("cardNumber").innerHTML = "8";
       console.log("You have got 8");
       console.log(random);
       break;
     case 9:
       card[8];
-      // document.getElementById("cardNumber").innerHTML = "9";
       console.log("You have got 9");
       console.log(random);
       break;
     case 10:
       card[9];
-      // document.getElementById("cardNumber").innerHTML = "10";
       console.log("You have got 10");
       console.log(random);
       break;
     case 11:
       card[10];
-      // document.getElementById("cardNumber").innerHTML = "Jack";
       console.log("You have got a Jack");
       console.log(random);
       break;
     case 12:
       card[11];
-      // document.getElementById("cardNumber").innerHTML = "Queen";
       console.log("You have got a Queen");
       console.log(random);
       break;
     case 13:
       card[12];
-      // document.getElementById("cardNumber").innerHTML = "King";
-      // addImageKing();
       console.log("You have got a King");
       console.log(random);
       break;
@@ -107,102 +93,85 @@ function genRandomCard() {
   genSecondCard();
 }
 
-// genRandomCard();
-
 function genSecondCard() {
-
-  // document.getElementsByClassName('card2')[0].style.visibility = 'hidden';
 
   random2 = Math.ceil(Math.random()*13);
 
   switch (random2) {
     case 1:
       secondCard[0];
-      // document.getElementById("cardNumber2").innerHTML = "Ace";
       addImageAce();
       console.log("You have got an ace");
       console.log(random2);
       break;
     case 2:
       secondCard[1];
-      // document.getElementById("cardNumber2").innerHTML = "2";
       addImage2();
       console.log("You have got 2");
       console.log(random2);
       break;
     case 3:
       secondCard[2];
-      // document.getElementById("cardNumber2").innerHTML = "3";
       addImage3();
       console.log("You have got 3");
       console.log(random2);
       break;
     case 4:
       secondCard[3];
-      // document.getElementById("cardNumber2").innerHTML = "4";
       addImage4();
       console.log("You have got 4");
       console.log(random2);
       break;
     case 5:
       secondCard[4];
-      // document.getElementById("cardNumber2").innerHTML = "5";
       addImage5();
       console.log("You have got 5");
       console.log(random2);
       break;
     case 6:
       secondCard[5];
-      // document.getElementById("cardNumber2").innerHTML = "6";
       addImage6();
       console.log("You have got 6");
       console.log(random2);
       break;
     case 7:
       secondCard[6];
-      // document.getElementById("cardNumber2").innerHTML = "7";
       addImage7();
       console.log("You have got 7");
       console.log(random2);
       break;
     case 8:
       secondCard[7];
-      // document.getElementById("cardNumber2").innerHTML = "8";
       addImage8();
       console.log("You have got 8");
       console.log(random2);
       break;
     case 9:
       secondCard[8];
-      // document.getElementById("cardNumber2").innerHTML = "9";
       addImage9();
       console.log("You have got 9");
       console.log(random2);
       break;
     case 10:
       secondCard[9];
-      // document.getElementById("cardNumber2").innerHTML = "10";
       addImage10();
       console.log("You have got 10");
       console.log(random2);
       break;
     case 11:
       secondCard[10];
-      // document.getElementById("cardNumber2").innerHTML = "Jack";
       addImageJack();
       console.log("You have got a Jack");
       console.log(random2);
       break;
     case 12:
       secondCard[11];
-      // document.getElementById("cardNumber2").innerHTML = "Queen";
       addImageQueen();
       console.log("You have got a Queen");
       console.log(random2);
       break;
     case 13:
       secondCard[12];
-      // document.getElementById("cardNumber2").innerHTML = "King";
       addImageKing();
       console.log("You have got a King");
       console.log(random2);
@@ -218,8 +187,6 @@ function genSecondCard() {
 
   var initialAudio = new Audio('assets/dealingCards.mp3');
   initialAudio.play();
-
-
 }
 
 function compare(num1, num2){
@@ -232,13 +199,11 @@ function compare(num1, num2){
   }
 }
 
-
 var higher = document.getElementById("higherbtn");
 higher.addEventListener("click", higherNumber);
 
 var lower = document.getElementById("lowerbtn");
 lower.addEventListener("click", lowerNumber);
-
 
 function higherNumber() {
 
@@ -272,7 +237,6 @@ function higherNumber() {
     resetGame();
   }
 }
-
 
 function lowerNumber() {
 
@@ -472,6 +436,5 @@ function addImage10() {
   ten.setAttribute("alt", "10");
   document.getElementById("cardNumber2").appendChild(ten);
 }
-
 
 });
